2fe6ee5a346c5d98e8c7674a494b8e5b80cbc4ba
Alexander Elias <alex.steven.elias@gmail.com>
2024-04-09 12:49:56 -0700

    crud api implemented

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
-# p
+# Note App Aws Cloud
 
 - https://g2g4il80hb.execute-api.us-east-1.amazonaws.com/Stage
 - https://aws.amazon.com/blogs/database/choosing-the-right-dynamodb-partition-key/
 - https://medium.com/@dimi_2011/developing-and-debugging-lambda-functions-with-dynamodb-locally-e8e1ee35fbb6
 
-- Alternative to SAM https://docs.aws.amazon.com/cdk/v2/guide/home.html
+### Deploy
 
-- Install Dynamodb Local
-  https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.DownloadingAndRunning.html
-  - Start DB
-    `java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb`
+```bash
+sam build && sam deploy --guided
+```
 
+### Run Locally
 
-- Build and start the app `sam build && sam local start-api`
+```bash
+# need for first time if not started on system
+sudo service docker start
+sam build && sam local start-api
+```
+
+### Delete
+
+```bash
+sam delete --stack-name notes
+```
--- notes/index.ts
+++ notes/index.ts
@@ -1,17 +1,23 @@
+import { DeleteItemCommand, DynamoDBClient, PutItemCommand, QueryCommand, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from 'aws-lambda';
-import { DynamoDBClient, PutItemCommand, QueryCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
-const user = 'alexelias';
+type User = string;
+interface Note {
+    // user: string,
+    note: string,
+    content: string,
+    created: number,
+    updated: number,
+}
+
 const TableName = 'notes-NotesDatabase-1BJHU2YMGWU32';
+const DynamoClient = new DynamoDBClient({ region: 'us-east-1' });
 
-const DynamoClient = new DynamoDBClient({
-    region: 'us-east-1',
-});
+const createNote = async (user: User, data: Note): Promise<APIGatewayProxyResult> => {
 
-const createNote = async (data: Record<string, string>): Promise<APIGatewayProxyResult> => {
-
-    if (!data.content) {
-        return { statusCode: 400, body: JSON.stringify({ message: 'body content required' }) };
+    if (typeof data.content !== 'string') {
+        return { statusCode: 400, body: JSON.stringify({ message: 'note content not valid' }) };
     }
 
     try {
@@ -32,23 +38,108 @@ const createNote = async (data: Record<string, string>): Promise<APIGatewayProxy
     return { statusCode: 200, body: JSON.stringify({ message: 'note created' }) };
 };
 
+const readNotes = async (user: User): Promise<APIGatewayProxyResult> => {
+
+    const items: Note[] = [];
+    try {
+        const result = await DynamoClient.send(new QueryCommand({
+            TableName,
+            ExpressionAttributeValues: {
+                ':u': { S: user }
+            },
+            ExpressionAttributeNames: {
+                '#u': 'user',
+            },
+            KeyConditionExpression:'#u = :u',
+        }));
+
+        if (result.Items) {
+            items.push(...result.Items.map(i => unmarshall(i) as Note));
+        }
+    } catch (error) {
+        return { statusCode: 500, body: JSON.stringify(error) };
+    }
+
+    return { statusCode: 200, body: JSON.stringify(items) };
+};
+
+const updateNote = async (user: User, data: Note): Promise<APIGatewayProxyResult> => {
+
+    if (!data.note) {
+        return { statusCode: 400, body: JSON.stringify({ message: 'note id required' }) };
+    }
+
+    if (typeof data.content !== 'string') {
+        return { statusCode: 400, body: JSON.stringify({ message: 'note type not valid' }) };
+    }
+
+    try {
+        await DynamoClient.send(new UpdateItemCommand({
+            TableName,
+            Key: {
+                user: { S: user },
+                note: { S: data.note },
+            },
+            ExpressionAttributeValues: {
+                ':updated': { N: `${Date.now()}` },
+                ':content': { S: data.content },
+            },
+            ExpressionAttributeNames: {
+                '#u': 'user',
+                '#n': 'note',
+                '#updated': 'updated',
+                '#content': 'content',
+            },
+            UpdateExpression: 'SET #updated = :updated, #content = :content',
+            ConditionExpression: 'attribute_exists(#u) AND attribute_exists(#n)',
+        }));
+    } catch (error) {
+        return { statusCode: 500, body: JSON.stringify(error) };
+    }
+
+    return { statusCode: 200, body: JSON.stringify({ message: 'updated note' }) };
+};
+
+const deleteNote = async (user: User, data: Note): Promise<APIGatewayProxyResult> => {
+
+    if (!data.note) return { statusCode: 400, body: JSON.stringify({ message: 'note required' }) };
+
+    try {
+        await DynamoClient.send(new DeleteItemCommand({
+            TableName,
+            Key: {
+                user: { S: user },
+                note: { S: data.note },
+            }
+        }));
+    } catch (error) {
+        return { statusCode: 500, body: JSON.stringify(error) };
+    }
+
+    return { statusCode: 200, body: JSON.stringify({ message: 'note deleted' }) };
+};
+
 export const main: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const method = event.httpMethod;
     const path = event.path;
+    const user = 'alexelias';
 
     let body;
     try {
         body = JSON.parse(event.body || '{}');
     } catch {
-        return { statusCode: 400, body: JSON.stringify({ message: 'body type not valid' }) };
+        return { statusCode: 400, body: JSON.stringify({ message: 'body not valid' }) };
     }
 
     if (method === 'POST' && path === '/note') {
-        return createNote(body);
+        return createNote(user, body);
+    } else if (method === 'GET' && path === '/notes') {
+        return readNotes(user);
+    } else if (method === 'PUT' && path === '/note') {
+        return updateNote(user, body);
+    } else if (method === 'DELETE' && path === '/note') {
+        return deleteNote(user, body);
     } else {
-        return {
-            statusCode: 404,
-            body: 'Not Found'
-        };
+        return { statusCode: 404, body: JSON.stringify({ message: 'Not Found' }) };
     }
 };
